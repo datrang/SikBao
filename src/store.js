b13f24b68ec8210ba5a0aeec0fa95a32a34319b4
KiranGurung <36213768+KiranGurung@users.noreply.github.com>
2018-05-02 12:01:49 -0700

    2 Panes!!!

--- src/store.js
+++ src/store.js
@@ -1,10 +1,16 @@
 export const foodtypes = ["Meats", "Vegetables", "Starch", "Fruits"];
 
 export const ingredients = [
-  {
-    id: "Chicken"
-  },
-  {
-    id: "Beef"
-  }
+    {
+        id: "Chicken",
+        foodtypes: "Fruits"
+     },
+    {
+        id: "Beef",
+        foodtypes: "Meats"
+     },
+    {
+        id: "Bread",
+        foodtypes: "Meats"
+    }
 ];
