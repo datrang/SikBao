a086cff1402f53c0452bf039357cf5b8d9e793ae
Michael <michaelee212@gmail.com>
2018-05-21 22:08:35 -0700

    sad
    asds

--- src/App.js
+++ src/App.js
@@ -2,40 +2,12 @@
  import logo from '.logo.svg';
  import './App.css';
 
-export default class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      todos: [],
-      currentTodo: ""
-    };
-  }
-
-  onInputChange = e => {
-    this.setState({currentTodo: e.target.value});
-  }
-
-  onClick = () => {
-    let todosCopy = this.state.todos.slice();
-    todosCopy.push(this.state.currentTodo);
-
-    this.setState({todos: todosCopy, currentTodo: "" });
-  }
-
+class App extends Component {
   render() {
-    let bulletedTodos = this.state.todos.map((e, i) => {
-      return (
-        <li key={i}>{e}</li>
-      );
-    });
     return(
       <div>
-        <input placeholder = "Enter todo" value = {this.state.currentTodo}
-          onChange = {this.onInputChange}/>
-        <button onClick = {this.onClick}>Add!</button>
-        <br />
-        { this.state.todos.length === 0 ? "None" : <ul>{bulletedTodos}</ul>}
-      </div>
+        <h1>Hello Worl<h1>
+      <di>
     );
   }
 }
